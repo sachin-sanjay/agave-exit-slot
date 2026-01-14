63209169fcf95bb0e25a9f4c132fe8e6915e755e
Sachin Samuel Sanjay <sachin.sanjay@icloud.com>
2026-01-14 21:33:33 +0400

    fixed

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use {
     agave_validator::admin_rpc_service,
-    std::Path
-}
+    std::path::Path
+};
 fn main() {
     let ledger_path=Path::new("/mnt/ledger");
-    let admin_client= match admin_rpc_service::connect(ledger_path){
-        Ok(client)=>client,
-        Err(_)=>{
-            println!("Failed to connect to admin RPC service");
-            return;
-        }   
-    }
-    println!("Hello, world!");
+    let admin_client= match admin_rpc_service::connect(ledger_path)?;
+    println!("connected to ledger");
 }
