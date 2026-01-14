6476460c2505c5acc8d2b1d6cc91e66652b08e2a
Sachin Samuel Sanjay <sachin.sanjay@icloud.com>
2026-01-14 22:02:36 +0400

    added async

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,9 @@ use {
     agave_validator::admin_rpc_service,
     std::path::Path
 };
-fn main() {
+#[tokio::main]
+async fn main() {
     let ledger_path=Path::new("/mnt/ledger");
-    let admin_client= match admin_rpc_service::connect(ledger_path).await{
-        Ok(client)=>client,
-        Err(e)=>{
-            eprintln!("failed to connect to ledger: {}",e);
-            return;
-        }
-    };  
+    let admin_client= match admin_rpc_service::connect(ledger_path).await?;
     println!("connected to ledger");
 }
