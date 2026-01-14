d3b55bee7ab77e29ac9edd3802cd2212af2643b4
Sachin Samuel Sanjay <sachin.sanjay@icloud.com>
2026-01-14 22:43:19 +0400

    cooking something

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use {
     agave_validator::admin_rpc_service,
-    std::path::Path
+    std::path::Path,
+    solana_rpc_client::rpc_client::RpcClient
 };
 #[tokio::main]
 async fn main() {
     let ledger_path=Path::new("/mnt/ledger");
     let admin_client= admin_rpc_service::connect(ledger_path).await.unwrap();
     println!("connected to ledger");
+    let pid=admin_client.pid().await.unwrap();
+    let rpc_endpoint=admin_client.rpc_addr().await.unwrap();
+    let rpc_client = RpcClient::new_socket(rpc_addr);
+    let mut identity = match rpc_client.get_identity() {
+        Ok(identity) => identity,
+        Err(err) => {
+            println!("Failed to get validator identity over RPC: {err}");
+            continue;
+        }
+    };
+    println!("Identity:{}", identity.to_string());
+    let current_slot=rpc_client.get_slot().unwrap();
+    println!("Current Slot:{}", current_slot);
+
 }
